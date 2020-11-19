b91f352c77ed9a68819127a08a6804bc01dfb828
NinaPran <70627636+NinaPran@users.noreply.github.com>
2020-11-19 11:26:43 +0100

    JS og closeing html

--- javascript/references.js
+++ javascript/references.js
@@ -11,13 +11,13 @@ var authorParagraph = document.getElementById("js-reference-author");
 var linkAnchor = document.getElementById("js-reference-link");
 var readParagraph = document.getElementById("js-reference-read");
 
-
 var references = [{
   title: "[1] Urban Transport Challenges",
   author: "Dr. Jean-Paul Rodrigue",
   linkText: "The Geography of Transport",
   link: "https://transportgeography.org/?page_id=4621",
   read: "27.10.2020"
+  
 },
 {
   title: "[2] Mindre husholdninger over hele landet",
@@ -26,6 +26,46 @@ var references = [{
   link: "https://www.ssb.no/fobhushold/",
   read: "27.10.2020"
 },
+{
+  title: "[3] Trafikantenes feil fører til dødsulykker",
+  author: "Author: Miriam Kvanvik ",
+  linkText: "Trygg Trafikk",
+  link: "https://www.tryggtrafikk.no/nyheter/trafikantenes-feil-forer-til-dodsulykker/",
+  read: "27.10.2020"
+},
+
+{
+  title: "[4] Hva påvirker utslipp til luft fra veitrafikk?",
+  author: "Author: Nadiya Fedoryshyn",
+  linkText: "Statistisk Sentralbyrå ",
+  link: "https://www.ssb.no/natur-og-miljo/artikler-og-publikasjoner/hva-pavirker-utslipp-til-luft-fra-veitrafikk",
+  read: "27.10.2020"
+},
+
+{
+  title: "[5] Gjennomsnittsbilen brukes bare 3,2 prosent av tiden ",
+  author: "Frank Williksen ",
+  linkText: "TV2.no ",
+  link: "https://www.tv2.no/a/10548724/",
+  read: "27.10.2020"
+},
+
+{
+  title: "[6] ABI Research Forecasts 8 Million Vehicles to Ship with SAE Level 3, 4 and 5 Autonomous Technology in 2025 ",
+  author: "ABI research",
+  linkText: "ABI research",
+  link: "https://www.abiresearch.com/press/abi-research-forecasts-8-million-vehicles-ship-sae-level-3-4-and-5-autonomous-technology-2025/",
+  read: "27.10.2020"
+},
+
+{
+  title: "[7] Fully automated vehicles: A cost of ownership analysis to inform early adoption",
+  author: "Zia Wadud",
+  linkText: "Link to Article",
+  link: "https://www.researchgate.net/publication/316644594_Fully_automated_vehicles_A_cost_of_ownership_analysis_to_inform_early_adoption",
+  read: "27.10.2020"
+},
+
 ];
 
 
